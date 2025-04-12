f09addbfe332394615c8400ee1bb27a4e2ad7db2
SEKAR KRISHNA S <114406150+skrixh@users.noreply.github.com>
2025-04-12 19:56:53 +0530

    Update 1768.py

--- 1768.py
+++ 1768.py
@@ -1,11 +1,11 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        merged = []
+        merged = []    # creating an empty list
 
-        for a,b in zip(word1, word2):
-            merged.append(a+b)
+        for a,b in zip(word1, word2):   #using zip fn and it'll be like (a,p) (b,q)
+            merged.append(a+b)     #appending the zip as a+b
 
-        merged.append(word1 [len(word2):])
-        merged.append(word2 [len(word1):])
+        merged.append(word1 [len(word2):])    #extra string characters
+        merged.append(word2 [len(word1):])     #extra string characters
 
-        return "".join(merged)
+        return "".join(merged)    #returns and joins the extra char
